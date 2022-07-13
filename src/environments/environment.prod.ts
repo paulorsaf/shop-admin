41c7178b48c379b34e4af20d642bb88ff0add7a0
Paulo Alves <paulorsaf@gmail.com>
2022-07-13 16:57:06 +0200

    Adjusting api url and build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import { environmentDefault } from "./environment.default";
 
 export const environment = {
   ...environmentDefault,
+  apiUrl: "https://southamerica-east1-shop-354211.cloudfunctions.net/shop-admin-server",
   production: true
 };
