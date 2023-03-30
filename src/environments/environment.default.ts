a7e97a96b950a7ff3c2a7ea0b5403369b89647f4
Paulo Alves <paulorsaf@gmail.com>
2023-03-30 09:40:33 +0200

    Removing service tax feature flag

--- src/environments/environment.default.ts
+++ src/environments/environment.default.ts
@@ -7,6 +7,5 @@ export const environmentDefault = {
         storageBucket: "shop-354211.appspot.com",
         messagingSenderId: "119837004113",
         appId: "1:119837004113:web:4dbd8ff53877d56ac78370"
-    },
-    showServiceTax: true
+    }
 }
